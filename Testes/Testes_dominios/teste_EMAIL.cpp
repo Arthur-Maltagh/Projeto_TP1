1cf93b86a989be68cca1d62b686049dba0a2197f
Arthur-Maltagh <rodriguesmaltaarthur@gmail.com>
2026-09-21 13:21:31 -0300

    commit teste p/ classe EMAIL

---
+++ Testes/Testes_dominios/teste_EMAIL.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+#include <stdexcept>
+#include "dominios/EMAIL.h"
+using namespace std;
+
+int main(){
+    try{
+        EMAIL Email;
+        string email;
+        cout << "digite seu EMAIL: ";
+        cin >> email;
+        Email.setEMAIL(email);
+        cout << "email: " << email << " armazenado com sucesso" << endl;
+    }
+    catch(const exception& e){
+        cout << "Erro:" << e.what() << endl;
+    }
+    return 0;
+}
+*/
